87a3b48f7132b7365f5649c3becc3d49a2c0fd0b
Pranav Mane <mane.pranav@outlook.com>
2025-10-01 14:04:07 +0530

    Added Calendar and User Profile Actions To All Controllers

--- P2PERP/Controllers/QualityController.cs
+++ P2PERP/Controllers/QualityController.cs
@@ -1,4 +1,5 @@
-﻿using P2PLibray.GRN;
+﻿using P2PLibray.Account;
+using P2PLibray.GRN;
 using P2PLibray.Quality;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,36 @@ namespace P2PERP.Controllers
     {
         BALQuality bal = new BALQuality();
 
+        #region Pranav Mane
+        public async Task<ActionResult> UserProfile()
+        {
+            var loginCheck = CheckLogin();
+            if (loginCheck != null)
+                return loginCheck;
+
+            BALAccount account = new BALAccount();
+            var acc = await account.UserProfileDetails(Session["StaffCode"].ToString());
+            return View(acc);
+        }
+
+        public ActionResult CheckLogin()
+        {
+            if (Session["StaffCode"] == null || string.IsNullOrWhiteSpace(Session["StaffCode"].ToString()))
+            {
+                return RedirectToAction("MainLogin", "Account");
+            }
+            return null;
+        }
+
+        public ActionResult Calender()
+        {
+            if (Session["StaffCode"] == null || string.IsNullOrWhiteSpace(Session["StaffCode"].ToString()))
+            {
+                return RedirectToAction("MainLogin", "Account");
+            }
+            return View();
+        }
+        #endregion
 
         #region Prashant
 
