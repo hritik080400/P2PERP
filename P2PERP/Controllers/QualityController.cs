8ea3f0b0d8c5bdd0797ca8d12859046ded27ae23
hritik080400 <94539105+hritik080400@users.noreply.github.com>
2025-10-05 22:15:28 +0530

    Revert "SBK"

--- P2PERP/Controllers/QualityController.cs
+++ P2PERP/Controllers/QualityController.cs
@@ -201,50 +201,13 @@ namespace P2PERP.Controllers
             return Json(new { data = grnList }, JsonRequestBehavior.AllowGet);
         }
 
+        #endregion Prashant
 
+        #region Rajlaxmi
 
-		// ========================== Graph Reports ==========================
-		// Confirmed Items Controller
-		[HttpGet]
-		public async Task<JsonResult> ConfirmedItemDetailsPSR(DateTime? startDate = null, DateTime? endDate = null)
-		{
-			try
-			{
-				var confirmedItems = await bal.ConfirmItemDetailsPSR(startDate, endDate);
-				return Json(new { success = true, data = confirmedItems }, JsonRequestBehavior.AllowGet);
-			}
-			catch (Exception ex)
-			{
-				return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
-			}
-		}
-
-		// Failed Items Controller
-		[HttpGet]
-		public async Task<JsonResult> FailedItemsGraphPR(DateTime? startDate = null, DateTime? endDate = null)
-		{
-			try
-			{
-				var failedItems = await bal.GetFailedItemsPR(startDate, endDate);
-				return Json(new { success = true, data = failedItems }, JsonRequestBehavior.AllowGet);
-			}
-			catch (Exception ex)
-			{
-				return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
-			}
-		}
-
-
-
-
-
-		#endregion Prashant
-
-		#region Rajlaxmi
-
-		// GET: QualityP2P
-		//  Default Index action
-		public ActionResult IndexRG()
+        // GET: QualityP2P
+        //  Default Index action
+        public ActionResult IndexRG()
         {
             return View();
         }
@@ -278,7 +241,7 @@ namespace P2PERP.Controllers
                 ViewBag.GRN = "";
             }
 
-            return PartialView("_NewTaskListRG"); //  always return same partial
+            return PartialView("_NewTaskListRG"); // ✅ always return same partial
         }
 
 
@@ -293,7 +256,7 @@ namespace P2PERP.Controllers
         public async Task<ActionResult> InspecFormRG(string id)
         {
             var data = await bal.InspecdetailsFormRG(id);
-            var newdata = data.FirstOrDefault(i => i.GrnItemCode == id);
+            var newdata = data.FirstOrDefault(i => i.ItemCode == id);
 
             if (newdata != null)
             {
@@ -302,7 +265,7 @@ namespace P2PERP.Controllers
                 ViewBag.ItemType = newdata.ItemType;
                 ViewBag.InspectionType = newdata.InspectionType;
                 ViewBag.PlanName = newdata.PlanName;
-                ViewBag.AddedDate = newdata.strAddedDate;
+                ViewBag.AddedDate = newdata.AddedDate;
                 ViewBag.Parameters = newdata.Parameters;
                 ViewBag.GRN = newdata.GRNCode;
                 ViewBag.Quantity = newdata.Quantity;
@@ -330,8 +293,8 @@ namespace P2PERP.Controllers
 
             var staffcode = Session["StaffCode"].ToString();
             await bal.ConfirmBtnInsStatusRG(id, sqc, Inf, staffcode);
-           
-            return Json(new { success = true });
+            await bal.UpdateGRNStatusRG(GRNCode);
+            return Json("");
         }
 
 
@@ -362,24 +325,24 @@ namespace P2PERP.Controllers
                     ViewBag.QCCode = QCCode;
 
 
-                    return PartialView("_NonconfirmPartialRG");
+                    return PartialView("_NonconfirmPartial");
                 }
 
                 ViewBag.Message = "No record found for ItemCode: " + itemcode;
-                return PartialView("_NonconfirmPartialRG");
+                return PartialView("_NonconfirmPartial");
             }
 
-            return PartialView("_NonconfirmPartialRG");
+            return PartialView("_NonconfirmPartial");
         }
 
-        //This is Function for getting data for update and also insert data into  tables
+        //This is Function for getting data for insert into two tables
         public async Task<ActionResult> finalNCRG(string GRNICode, string SQC, string INF, string FQC, string ROR, string QC, string STF, string GRNCode)
         {
             var staffcode = Session["StaffCode"].ToString();
-            String QCCODE = await bal.GetQcCodeRG(GRNICode);
-            await bal.InitiatebtnstatusRG(GRNICode, INF, SQC, staffcode);
-            await bal.insertQFitemsRG(FQC, QCCODE, STF, ROR);
 
+            await bal.InitiatebtnstatusRG(QC, GRNICode, INF, SQC, staffcode);
+            await bal.insertQFitemsRG(FQC, QC, STF, ROR);
+            await bal.UpdateGRNStatusRG(GRNCode);
             return Json("");
 
         }
